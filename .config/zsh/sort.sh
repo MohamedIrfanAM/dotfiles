62c87cfadd1ce8325df269a68985372f2082509a
MohamedIrfanAM <i4irfan777@gmail.com>
2021-04-16 19:46:46 +0530

    cleaning cp directory script

---
+++ .config/zsh/sort.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+mv /run/media/irfan/Programming/cp/At* /run/media/irfan/Programming/cp/atcoder
+mv /run/media/irfan/Programming/cp/Ha* /run/media/irfan/Programming/cp/hackerrank
+mv /run/media/irfan/Programming/cp/Codeforc* /run/media/irfan/Programming/cp/codeforces
+mv /run/media/irfan/Programming/cp/CodeShef* /run/media/irfan/Programming/cp/codechef
+rm -fr /run/media/irfan/Programming/cp/*.exe
+clear
