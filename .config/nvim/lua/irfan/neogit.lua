85b87de42027adcc3d7d5604f29c28c6c6e22b89
Mohamed Irfan <i4irfan777@gmail.com>
2024-02-17 22:48:51 +0530

    neogit update

--- .config/nvim/lua/irfan/neogit.lua
+++ .config/nvim/lua/irfan/neogit.lua
@@ -13,7 +13,7 @@ function M.config()
   require("neogit").setup {
     disable_signs = false,
     -- disable_hint = true,
-    disable_context_highlighting = false,
+    disable_context_highlighting = true,
     disable_commit_confirmation = true,
     disable_insert_on_commit = "auto",
     -- Neogit refreshes its internal state after specific events, which can be expensive depending on the repository size.
