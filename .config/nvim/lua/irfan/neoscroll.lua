f09b1196abca27d1e8b5a12decea592cb69ddf42
Mohamed Irfan <i4irfan777@gmail.com>
2024-02-17 20:36:18 +0530

    update neoscroll duration scroll

--- .config/nvim/lua/irfan/neoscroll.lua
+++ .config/nvim/lua/irfan/neoscroll.lua
@@ -15,8 +15,8 @@ function M.config()
     -- All these keys will be mapped to their corresponding default scrolling animation
     mappings = { "<C-u>", "<C-d>", "<C-b>", "<C-f>", "<C-y>", "<C-e>", "zt", "zz", "zb", "C-k", "C-j" },
     hide_cursor = true, -- Hide cursor while scrolling
-    stop_eof = true, -- Stop at <EOF> when scrolling downwards
-    respect_scrolloff = false, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
+    stop_eof = false, -- Stop at <EOF> when scrolling downwards
+    respect_scrolloff = true, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
     cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
     easing_function = nil, -- Default easing function
     pre_hook = nil, -- Function to run before the scrolling animation starts
@@ -26,10 +26,10 @@ function M.config()
 
   local t = {}
   -- Syntax: t[keys] = {function, {function arguments}}
-  t["<C-u>"] = { "scroll", { "-vim.wo.scroll", "true", "250" } }
-  t["<C-d>"] = { "scroll", { "vim.wo.scroll", "true", "250" } }
-  t["<C-k>"] = { "scroll", { "-vim.wo.scroll", "true", "250" } }
-  t["<C-j>"] = { "scroll", { "vim.wo.scroll", "true", "250" } }
+  t["<C-u>"] = { "scroll", { "-vim.wo.scroll", "true", "50" } }
+  t["<C-d>"] = { "scroll", { "vim.wo.scroll", "true", "50" } }
+  t["<C-k>"] = { "scroll", { "-vim.wo.scroll", "true", "50" } }
+  t["<C-j>"] = { "scroll", { "vim.wo.scroll", "true", "50" } }
   t["<C-b>"] = { "scroll", { "-vim.api.nvim_win_get_height(0)", "true", "450" } }
   t["<C-f>"] = { "scroll", { "vim.api.nvim_win_get_height(0)", "true", "450" } }
   t["<C-y>"] = { "scroll", { "-0.10", "false", "100" } }
