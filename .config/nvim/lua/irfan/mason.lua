9476066db14894dea3eef7ad89880449fe97f8da
Mohamed Irfan <i4irfan777@gmail.com>
2024-02-18 15:33:55 +0530

    remove prettierd from lspconfig because of it exist in nullls

--- .config/nvim/lua/irfan/mason.lua
+++ .config/nvim/lua/irfan/mason.lua
@@ -1,33 +1,32 @@
 local M = {
-  "williamboman/mason-lspconfig.nvim",
-  dependencies = {
-    "williamboman/mason.nvim",
-    "nvim-lua/plenary.nvim",
-  },
+	"williamboman/mason-lspconfig.nvim",
+	dependencies = {
+		"williamboman/mason.nvim",
+		"nvim-lua/plenary.nvim",
+	},
 }
 
 function M.config()
-  local servers = {
-    "lua_ls",
-    "cssls",
-    "html",
-    "tsserver",
-    "tailwindcss",
-    "eslint",
-    "prettierd",
-    "bashls",
-    "jsonls",
-  }
+	local servers = {
+		"lua_ls",
+		"cssls",
+		"html",
+		"tsserver",
+		"tailwindcss",
+		"eslint",
+		"bashls",
+		"jsonls",
+	}
 
-  require("mason").setup {
-    ui = {
-      border = "rounded",
-    },
-  }
+	require("mason").setup({
+		ui = {
+			border = "rounded",
+		},
+	})
 
-  require("mason-lspconfig").setup {
-    ensure_installed = servers
-  }
+	require("mason-lspconfig").setup({
+		ensure_installed = servers,
+	})
 end
 
 return M
