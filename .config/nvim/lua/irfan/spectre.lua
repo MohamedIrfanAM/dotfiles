126ed1b06d9c9d22664698360f2993421409d18e
Mohamed Irfan <i4irfan777@gmail.com>
2024-02-23 08:39:57 +0530

    change spectre keybind

--- .config/nvim/lua/irfan/spectre.lua
+++ .config/nvim/lua/irfan/spectre.lua
@@ -4,13 +4,26 @@ local M = {
 }
 
 function M.config()
-	require("spectre").setup()
+	require("spectre").setup({
+		mapping = {
+			["run_current_replace"] = {
+				map = "<leader>R",
+				cmd = "<cmd>lua require('spectre.actions').run_current_replace()<CR>",
+				desc = "replace current line",
+			},
+			["run_replace"] = {
+				map = "<leader>A",
+				cmd = "<cmd>lua require('spectre.actions').run_replace()<CR>",
+				desc = "replace all",
+			},
+		},
+	})
 	local wk = require("which-key")
 	wk.register({
 		["<leader>ss"] = { '<cmd>lua require("spectre").toggle()<CR>', "Spectre" },
 		["<leader>sw"] = { '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', "Search word" },
 		["<leader>sf"] = {
-			'<cmd>lua require("spectre").open_file_search({select_word=true})<CR>',
+			'<cmd>lua require("spectre").open_file_search()<CR>',
 			"Search current file",
 		},
 	})
