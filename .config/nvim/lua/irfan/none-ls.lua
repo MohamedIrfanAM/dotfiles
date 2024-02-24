8b4e7a46992973c76e0fbe64abc8e4cc929fe717
Mohamed Irfan <i4irfan777@gmail.com>
2024-02-24 19:37:42 +0530

    remove autoformat on save

--- .config/nvim/lua/irfan/none-ls.lua
+++ .config/nvim/lua/irfan/none-ls.lua
@@ -22,18 +22,6 @@ function M.config()
 			formatting.goimports_revisor,
 			formatting.golines,
 		},
-		on_attach = function(client, bufnr)
-			if client.supports_method("textDocument/formatting") then
-				vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-				vim.api.nvim_create_autocmd("BufWritePre", {
-					group = augroup,
-					buffer = bufnr,
-					callback = function()
-						vim.lsp.buf.format()
-					end,
-				})
-			end
-		end,
 	})
 end
 
