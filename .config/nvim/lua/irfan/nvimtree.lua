3bbe1485d9eb49d86be1a8866852a690655e3389
Mohamed Irfan <i4irfan777@gmail.com>
2024-02-29 17:46:16 +0530

    change nvimtree createfilekeybind

--- .config/nvim/lua/irfan/nvimtree.lua
+++ .config/nvim/lua/irfan/nvimtree.lua
@@ -21,7 +21,7 @@ function M.config()
     api.config.mappings.default_on_attach(bufnr)
 
     vim.keymap.set("n", "l", api.node.open.edit, opts "Open")
-    vim.keymap.set("n", "n", api.fs.create, opts "Create new file or directory")
+    vim.keymap.set("n", "a", api.fs.create, opts "Create new file or directory")
     vim.keymap.set("n", "h", api.node.navigate.parent_close, opts "Close Directory")
     vim.keymap.set("n", "v", api.node.open.vertical, opts "Open: Vertical Split")
     vim.keymap.del("n", "<C-k>", { buffer = bufnr })
