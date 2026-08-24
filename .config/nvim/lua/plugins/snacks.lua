8fd175289519e203a8adb629115a72a97c90090c
irfan.m <irfan.m@media.net>
2026-08-24 23:54:42 +0530

    search hidden files as well nvim

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -6,6 +6,12 @@ return {
         explorer = {
           hidden = true,
         },
+        files = {
+          hidden = true,
+        },
+        grep = {
+          hidden = true,
+        },
       },
     },
   },
